1fbd997aae6cdfaa1cf0aae04c3122ff4c4df950
SperryAG <agsperry21@gmail.com>
2017-01-23 00:28:33 -0800

    Revert "Built the Result class"
    This reverts commit e7d72e70350ff2ace859e99a49b204b9d99677c0.

--- Result.java
+++ Result.java
@@ -1,39 +1,5 @@
 package MazeGenerator;
 
 class Result {
-	// Variables
-	private int robotCount;
-	private int completionTime; // In seconds
-	
-	// Constructors
-	public Result() {
-		robotCount = -1;
-		completionTime = -1;
-	}
-	public Result(int robotCount, int completionTime) {
-		this.robotCount = robotCount;
-		this.completionTime = completionTime;
-	}
-	
-	// Methods
-	public void setRobotCount(int robotCount) {
-		this.robotCount = robotCount;
-	}
-	public int getRobotCount() {
-		return robotCount;
-	}
-	public void setCompletionTime(int completionTime) {
-		this.completionTime = completionTime;
-	}
-	public int getCompletionTime() {
-		return completionTime;
-	}
-	
-	public String toString() {
-		String output = "";
-		
-		output += "<Result>(" + robotCount + "," + completionTime + ")</Result>";
-		
-		return output;
-	}
+
 }
