967f1e69d5162884345d979274b90d11b54759cc
Steven Chow <schow3@uci.edu>
2017-01-24 10:06:46 -0800

    Test Commit again

--- MazeGenerator/MazeGenerator.java
+++ MazeGenerator/MazeGenerator.java
@@ -4,6 +4,7 @@ public class MazeGenerator {
 	public static void main(String[] args) {
 		Maze maze;
 		//Oh test commit
+		//Steven's test commit again
 		maze = new Maze("FirstMaze",12,42);
 		System.out.println(maze.toString());
 		maze.toFile();
